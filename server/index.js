af4ab5292ddf3998e5ca6f42f6ed45674b1b5757
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2023-01-19 12:48:33 +0200

    config ports server

--- server/index.js
+++ server/index.js
@@ -4,11 +4,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const server = http.createServer(app)
 
-server.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
-})
-
-/* const PORT = config.PORT || 3001
+const PORT = config.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
-}) */
+})
