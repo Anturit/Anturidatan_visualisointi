dcaef67e0a16919cfcb82bf32979e08332395fc5
riikkayoki <74421508+riikkayoki@users.noreply.github.com>
2023-01-25 19:05:42 +0200

    frontend registraion form

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -1,6 +1,50 @@
+import { useState } from 'react'
 
+const RegisterForm = () => {
+  const roles = ['Admin', 'User']
 
-const RegisterForm = () => {return ('Hello World')}
+  const [newUser, setNewUser] = useState('')
+  const [newPassword, setNewPassword] = useState('')
+  const [selectedRole, setSelectedRole] = useState(roles[0])
+
+  const submit = () => {
+    console.log(newUser)
+    console.log(newPassword)
+    console.log(selectedRole)
+    console.log('tähän handleRegistration')
+  }
+
+  return (
+    <div>
+      <form>
+        <div>
+          <h4>Rekisteröi uusi käyttäjä</h4>
+          <label>Käyttäjänimi</label>
+        </div>
+        <input value={newUser} onChange={(e) => setNewUser(e.target.value)}/>
+        <div>
+          <div>
+            <label>Salasana</label>
+          </div>
+          <input value={newPassword} onChange={(e) => setNewPassword(e.target.value)}/>
+        </div>
+        <label>Rooli</label>
+        <div>
+          <select value={selectedRole} onChange ={(e) => setSelectedRole(e.target.value)}>
+            {roles.map((value) => (
+              <option value={value} key={value}>
+                {value}
+              </option>
+            ))}
+          </select>
+        </div>
+        <p>
+          <button type="submit" onClick={submit}>Lisää uusi käyttäjä</button>
+        </p>
+      </form>
+    </div>
+  )
+}
 
 export default RegisterForm
 
