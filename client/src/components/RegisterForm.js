df4c31723015a1f4ff09d061de10867c2e2c85fc
riikkayoki <74421508+riikkayoki@users.noreply.github.com>
2023-01-25 19:31:50 +0200

    frontend form + firstname + surname

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -3,36 +3,43 @@ import { useState } from 'react'
 const RegisterForm = () => {
   const roles = ['Admin', 'User']
 
-  const [newUser, setNewUser] = useState('')
+  const [newFirstName, setNewFirstName] = useState('')
+  const [newSurname, setNewSurname] = useState('')
+  const [newUsername, setNewUsername] = useState('')
   const [newPassword, setNewPassword] = useState('')
   const [selectedRole, setSelectedRole] = useState(roles[0])
-
   const submit = () => {
-    console.log(newUser)
-    console.log(newPassword)
-    console.log(selectedRole)
     console.log('tähän handleRegistration')
   }
 
   return (
     <div>
       <form>
+        <h4>Rekisteröi uusi käyttäjä</h4>
+        <label></label>
+        <label>Etunimi</label>
         <div>
-          <h4>Rekisteröi uusi käyttäjä</h4>
-          <label>Käyttäjänimi</label>
+          <input value={newFirstName} id='newFirstName' onChange={(e) => setNewFirstName(e.target.value)}/>
         </div>
-        <input value={newUser} onChange={(e) => setNewUser(e.target.value)}/>
+        <label>Sukunimi</label>
+        <div>
+          <input value={newSurname} id='newSurname' onChange={(e) => setNewSurname(e.target.value)}/>
+        </div>
+        <label>Käyttäjätunnus</label>
+        <div>
+          <input value={newUsername} id='newUsername' onChange={(e) => setNewUsername(e.target.value)}/>
+        </div>
         <div>
           <div>
             <label>Salasana</label>
           </div>
-          <input value={newPassword} onChange={(e) => setNewPassword(e.target.value)}/>
+          <input type={'password'} id='newPassword' value={newPassword} onChange={(e) => setNewPassword(e.target.value)}/>
         </div>
         <label>Rooli</label>
         <div>
           <select value={selectedRole} onChange ={(e) => setSelectedRole(e.target.value)}>
             {roles.map((value) => (
-              <option value={value} key={value}>
+              <option value={value} key={value} id='selectedRole'>
                 {value}
               </option>
             ))}
