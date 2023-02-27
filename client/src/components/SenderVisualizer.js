31131e48ea8c87984d0be0f9bd8114fcb1ce30d1
Vili Sirkeoja <vili.sirkeoja@gmail.com>
2023-02-27 22:01:29 +0200

    Modify UI of sender data visualization

--- client/src/components/SenderVisualizer.js
+++ client/src/components/SenderVisualizer.js
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { SensorChart } from './SensorChart'
 import { formatData, getMeasurementParameters, getSmallSensorIds } from '../utils/senderDataHandler'
 
@@ -10,12 +9,6 @@ const SenderVisualizer = ({ id, logs }) => {
    * @returns {JSX.Element} JSX element of device's sensor charts
    */
 
-  //React hook containing info about showing/not showing the charts
-  const [detailed, setDetailed] = useState(false)
-
-  const hideWhenDetailed = { display: detailed ? 'none' : '' }
-  const showWhenDetailed = { display: detailed ? '' : 'none' }
-
   const measurementParameters = getMeasurementParameters(logs)
 
   const bigSensorIds = getSmallSensorIds(logs)
@@ -24,18 +17,12 @@ const SenderVisualizer = ({ id, logs }) => {
   const smallSensorIds = getSmallSensorIds(logs, true)
   const smallSensorData = formatData(logs, measurementParameters)
 
-  const toggleDetails = () => {
-    //Function that toggles the visibility of the charts
-    setDetailed(!detailed)
-  }
-
   return (
     <div className='sensor-chart-group'>
-      <div style={hideWhenDetailed}>
-        <p><b>{id} </b><button onClick={toggleDetails}>näytä</button></p>
+      <div>
+        <p><b>{id}</b></p>
       </div>
-      <div style={showWhenDetailed}>
-        <p><b>Laite-ID {id}:n logit </b> <button onClick={toggleDetails}>piilota</button></p>
+      <div>
         {measurementParameters.filter(parameter => parameter !== 'measurement').map(parameter =>
           <SensorChart id='normal-sensor-chart' key={parameter} parameter={parameter} ids={bigSensorIds} logs={bigSensorData} />
         )}
