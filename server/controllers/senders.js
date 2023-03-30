a743304019ebfe6e4dc9fe8d307a6e89a1edc068
Johannes Elonheimo <37146266+elonheimo@users.noreply.github.com>
2023-03-30 11:38:42 +0300

    admin route to bulk inset senders

--- server/controllers/senders.js
+++ server/controllers/senders.js
@@ -25,4 +25,18 @@ senderRouter.get('/:id', async (request, response) => {
   response.json(device)
 })
 
+senderRouter.post('/', adminCredentialsValidator, async (request, response) => {
+  const dataPoints = await request.body
+  if (!Array.isArray(dataPoints))
+    return response.status(400).json({ error: 'request content must be an array' })
+  dataPoints.forEach(
+    dataPoint => dataPoint['date'] = new Date(dataPoint.date)
+  )
+  const insertResult = await Sender.insertMany(dataPoints, { rawResult: true })
+  if (insertResult.insertedCount !== dataPoints.length) {
+    return response.status(400).json({ error: 'some data points were not inserted' })
+  }
+  return response.status(201).end()
+})
+
 module.exports = senderRouter
