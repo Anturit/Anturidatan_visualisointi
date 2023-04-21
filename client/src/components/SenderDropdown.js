ea6acd8dbc7903272857ba9f0231efb62738c7a1
Johannes Elonheimo <37146266+elonheimo@users.noreply.github.com>
2023-04-21 14:30:46 +0300

    REFACTOR sender dropdown to mui

--- client/src/components/SenderDropdown.js
+++ client/src/components/SenderDropdown.js
@@ -1,18 +1,29 @@
-const SenderDropdown = ({ senderDeviceIds, setSelectedSenderId })  => {
+import {
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
+} from '@mui/material'
+
+const SenderDropdown = ({ senderDeviceIds, setSelectedSenderId }) => {
 
   const handleSenderChange = (event) => {
     event.preventDefault()
     setSelectedSenderId(event.target.value)
   }
 
-  return (
-    <div>
-      <label htmlFor='senders'>Valitse lähetin:</label>
-      <select name='senders' id='senders' onChange={handleSenderChange} data-cy='senderDropdown'>
-        {senderDeviceIds.map(sender => <option key={sender} value={sender}>{sender}</option>)}
-      </select>
-    </div>
-  )
+  return <FormControl variant='standard'>
+    <InputLabel id='senderDropdown-label'>Valittu lähetin</InputLabel>
+    <Select
+      labelId='senderDropdown-label'
+      data-cy='senderDropdown'
+      onChange={handleSenderChange}
+      label='Valittu lähetin'
+      defaultValue={senderDeviceIds[0]}
+    >
+      {senderDeviceIds.map(sender => <MenuItem key={sender} value={sender}>{sender}</MenuItem>)}
+    </Select>
+  </FormControl>
 }
 
 export default SenderDropdown
