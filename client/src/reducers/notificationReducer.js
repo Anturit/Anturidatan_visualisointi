a6f7f01147fc1e3ca98dadeb10271c93eb77b62a
Johannes Elonheimo <37146266+elonheimo@users.noreply.github.com>
2023-04-21 13:00:09 +0300

    FIX initial state

--- client/src/reducers/notificationReducer.js
+++ client/src/reducers/notificationReducer.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = { message: '' }
+const initialState = { message: '', type: 'info' }
 
 const notificationSlice = createSlice({
   name: 'notification',
